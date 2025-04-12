cedd616f9ff7411c782f58e0fa9748c89fa7c85c
prabhupachisia <prabhu.pachisia2023@vitstudent.ac.in>
2025-04-13 02:08:18 +0530

    Changed file names

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Home from './Pages/HomePage/Homepage';
-import Login from './Pages/Login/login';
+import Login from './Pages/Login/Login';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import DocRegister from './Pages/DocRegister/DocRegister';
 import HosRegister from './Pages/HosRegister/HosRegister';
